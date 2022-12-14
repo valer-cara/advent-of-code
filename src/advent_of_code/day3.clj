2e9f0cdb613666510066b6ca163e6e912427ff8a
Valer Cara <valer@griminal.net>
2022-12-14 22:30:53 +0200

    Day 3, part 1

--- src/advent_of_code/day3.clj
+++ src/advent_of_code/day3.clj
@@ -1,17 +1,57 @@
 (ns advent-of-code.day3
   (:require [clojure.string :as str]))
 
-(defn solve []
-  ;; TODO figure out this inline defs, they suck indeed, (let?)
-  (def input (str/split (slurp "./src/advent_of_code/day3.input.txt") #"\n"))
+(def input (str/split (slurp "./src/advent_of_code/day3.input.txt") #"\n"))
 
-  (list
-   "part 1" 1234
-   "part 2" 9999))
+(defn prio [chr]
+  (let [base (if (>= (int chr) (int \a))
+               (int \a)
+               (- (int \A) 26))]
+    (inc (- (int chr) base)))
+  )
 
-#_(solve)
+(defn revprio [prio]
+  (let [base (if (< prio 27)
+               (dec (int \a))
+               (- (int \A) 27))]
+    (char (+ prio base)))
+  )
+
+(assert (= (revprio (prio \m)) \m))
+(assert (= (revprio (prio \M)) \M))
+
+(defn encode [pr left]
+  ;; NOTE: cond-> is my favorite function so far!
+  (cond-> (* 10 pr) left inc))
+
+(def encode-all (fn [len idx val]
+                  (encode (prio val) (< idx (/ len 2)))))
+
+;; Returns the first value in the reduction that's followed by it's consecutive
+(def reduce-consecutive
+  (fn [acc val]
+    (if (= val (inc acc))
+      (reduced (int (/ val 10)))
+      val)))
+
+(comment 
+  (reduce reduce-consecutive [4 5])
+  (reduce reduce-consecutive [4 8 10 15 15 20 22 23 50 55 60]))
+
+(defn solve-single [in]
+  (reduce reduce-consecutive
+          (sort (map-indexed (partial encode-all (count in)) in))))
 
 (comment
-  (reductions + [1 1 2 3 4 5 6])
+  (revprio (solve-single "vJrwpWtwJgWrhcsFMMfFFhFp"))
+  (revprio (solve-single "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"))
+  (revprio (solve-single "PmmdzqPrVvPwwTWBwg"))
+  (revprio (solve-single "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"))
+  (revprio (solve-single "ttgJtRGJQctTZtZT"))
+  (revprio (solve-single "CrZsJsPPZsGzwwsLwLmpwMDw")))
 
-  )
+(defn solve []
+  (apply + (map solve-single input)))
+
+#_(solve)
+;; => 7917
