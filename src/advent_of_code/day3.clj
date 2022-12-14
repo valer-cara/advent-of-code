299586b46a7d69dfc5346bce1db53f7863f6c00a
Valer Cara <valer@griminal.net>
2022-12-14 09:25:53 +0200

    readme update

--- src/advent_of_code/day3.clj
+++ src/advent_of_code/day3.clj
@@ -5,12 +5,6 @@
   ;; TODO figure out this inline defs, they suck indeed, (let?)
   (def input (str/split (slurp "./src/advent_of_code/day3.input.txt") #"\n"))
 
-  ;; TODO: use tests to provide input like
-  ;; https://github.com/rjray/advent-2020-clojure/blob/master/test/advent_of_code/day01_test.clj
-
-  ;; TODO: find a project, input pattern looking at
-  ;; https://github.com/Bogdanp/awesome-advent-of-code#clojure
-
   (list
    "part 1" 1234
    "part 2" 9999))
